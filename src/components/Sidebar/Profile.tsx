2e4c8126f111f97ee72b7843e9cffce069bb4b79
Tarcisio Couto <tarcisiocouto@MacBook-Air-de-Tarcisio.local>
2024-01-25 14:36:55 -0300

    Adicionado novo projeto XYZ

--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -1,24 +1,25 @@
 import { LogOut } from 'lucide-react'
+import { Button } from '../Button'
 
 export function Profile() {
   return (
-    <div className="grid grid-cols-profile items-center gap-3">
+    <div className="grid grid-cols-profile items-center gap-4 mt-2">
       <img
         src="https://github.com/TarCouto.png"
         className="h-10 w-10 rounded-full"
         alt=""
       />
       <div className="flex flex-col truncate">
-        <span className="text-sm font-semibold text-zinc-700">
+        <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
           Tarcisio Couto
         </span>
-        <span className="truncate text-sm text-zinc-500">
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
           Tarcisiocouto.rp@gmail.com
         </span>
       </div>
-      <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
+      <Button type="button" variant="ghost">
         <LogOut className="h-5 w-5 text-zinc-500" />
-      </button>
+      </Button>
     </div>
   )
 }
